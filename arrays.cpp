87b2ca954d75b3e1e900bcc1f9fbac809d894c21
n1oftheabove <35739070+n1oftheabove@users.noreply.github.com>
2018-06-28 00:23:30 +0200

    Add files via upload

---
+++ arrays.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstdlib>
+#include<math.h>
+using namespace std;
+
+//int findmax( int obj);
+
+int main(){
+int myarray[10];
+cout<<"Geben Sie 10 ganze Zahlen ein!"<<endl;
+for(int i=0, i<10, i++){
+cout << i+1 <<". te Zahl?"<<endl;
+cin>>myarray[i];
+}
+system("PAUSE");
+
+
+}
+
+//int findmax( int obj){
+
+
+
+//}
