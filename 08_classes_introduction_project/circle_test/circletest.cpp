20a0c4f64dcab02255ccf09e8b1ed028f5d84ee6
n1oftheabove <35739070+n1oftheabove@users.noreply.github.com>
2018-07-02 14:56:34 +0200

    Rename 08_classes_introduction_project/circletest.cpp to 08_classes_introduction_project/circle_test/circletest.cpp

--- 08_classes_introduction_project/circle_test/circletest.cpp
+++ 08_classes_introduction_project/circle_test/circletest.cpp
@@ -13,33 +13,33 @@ int main(){
 
 	Circle c1;
 	Point p1;	// Kreismittelpunkt
-	Point p2; //Punkt, der überprüft werden soll
+	Point p2; //Punkt, der Ã¼berprÃ¼ft werden soll
 	p1.x_coor = 0;
 	p1.y_coor = 0;
 	p2.x_coor = 0;
 	p2.y_coor = 0;
-	c1.p = p1; // Wichtig! Zu c1 gehört ja noch .p!
+	c1.p = p1; // Wichtig! Zu c1 gehÃ¶rt ja noch .p!
 
 	cout << "Geben Sie die x-Koordinate des Kreismittelpunktes an.\n" <<endl;
 	if(!(cin >> p1.x_coor)){
-		cout << "ungültige Eingabe" <<endl;
+		cout << "ungÃ¼ltige Eingabe" <<endl;
 	};
 	cout << "Geben Sie die y-Koordinate des Kreismittelpunktes an.\n" <<endl;
 	if(!(cin >> p1.y_coor)){
-		cout << "ungültige Eingabe" <<endl;
+		cout << "ungÃ¼ltige Eingabe" <<endl;
 	};
 	cout << "Geben Sie den Radius des Kreises an.\n" <<endl;
 	if(!(cin>> c1.radius)){
-		cout << "ungültige Eingabe" <<endl;
+		cout << "ungÃ¼ltige Eingabe" <<endl;
 	};
-	cout << "Geben Sie jetzt einen Punkt an! Ich werde überprüfen, ob dieser auf, in oder außerhalb des Kreises liegt.\n" <<endl;
+	cout << "Geben Sie jetzt einen Punkt an! Ich werde Ã¼berprÃ¼fen, ob dieser auf, in oder auÃŸerhalb des Kreises liegt.\n" <<endl;
 	cout << "x-Koordinate des Punktes?" << endl;
 	if(!(cin >> p2.x_coor)){
-		cout << "ungültige Eingabe" <<endl;
+		cout << "ungÃ¼ltige Eingabe" <<endl;
 	};
 	cout << "y-Koordinate des Punktes?" << endl;
 	if(!(cin >> p2.y_coor)){
-		cout << "ungültige Eingabe" <<endl;
+		cout << "ungÃ¼ltige Eingabe" <<endl;
 	};
 
 	if(c1.isin(p2)) cout << "Punkt liegt innerhalb" << endl;
