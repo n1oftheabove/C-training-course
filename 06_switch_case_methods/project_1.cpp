006468b2061cf1998c5b571fe3c4ed94ce1924bf
n1oftheabove <35739070+n1oftheabove@users.noreply.github.com>
2018-07-02 14:12:26 +0200

    Rename Quelle.cpp to 06_switch_case_methods/project_1.cpp

--- 06_switch_case_methods/project_1.cpp
+++ 06_switch_case_methods/project_1.cpp
@@ -10,21 +10,21 @@ using namespace std;
 int main(){
 	int x = 34;
 
-	//Arrays, Elemente müssen alle vom gleichen Typ sein, Indexe von 0 bis n-1; negative Indexe oder floats nicht erlaubt
+	//Arrays, Elemente mÃ¼ssen alle vom gleichen Typ sein, Indexe von 0 bis n-1; negative Indexe oder floats nicht erlaubt
 	int geb_datum[3]; //0-te, 1-te, und 2-te
 	geb_datum[0] = 19; geb_datum[1] = 1; geb_datum[2] = 1981;
 	// Elemente auf der Konsole ausgeben
 	cout << geb_datum[0];
 	cout << geb_datum[1];
 	cout << geb_datum[2];
-	//oder kürzer
+	//oder kÃ¼rzer
 	for(int i = 0; i < 3; i++) cout << geb_datum[i]; //lokale Variabel i ist an Block gebunden
 	//mit while
 	int i = 0;
 	while(i<=2){cout<<geb_datum[0];i++;}
 	
 	do{cout<<geb_datum[0]; i++;
-	}while(i<=2);				//Anweisungen werden mindestens einmal ausgeführt, passt z.b. bei Programmen, wenn dem User ein Menü präsentiert werden soll, unabhängig, ob seine darauffolgende EIngabe korrekt ist oder nicht
+	}while(i<=2);				//Anweisungen werden mindestens einmal ausgefÃ¼hrt, passt z.b. bei Programmen, wenn dem User ein MenÃ¼ prÃ¤sentiert werden soll, unabhÃ¤ngig, ob seine darauffolgende EIngabe korrekt ist oder nicht
 
 system("PAUSE");
 return 0;
