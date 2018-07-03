10e8d959217e38e303d9600d6d2154fd91375a93
n1oftheabove <35739070+n1oftheabove@users.noreply.github.com>
2018-07-04 01:07:28 +0200

    Add files via upload

---
+++ 29_PROJECT_library_database/Angestellter.h
@@ -0,0 +1,11 @@
+/*#include "Mitarbeiter.h"
+#include "Datum.h"
+
+class Angestellter:public Mitarbeiter{
+private:
+	Datum eingestelltseit;
+public:
+	Angestellter();
+	Angestellter(string, string, string, string, int, string, Datum);
+	~Angestellter();
+};*/
